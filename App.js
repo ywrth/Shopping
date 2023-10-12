fb5c35fc0d9a3c0b84732ac0ebab78c55647db05
ywrth <zuatworknow@gmail.com>
2023-10-12 11:59:59 +0200

    shopping app

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import react Navigation
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-export default function App() {
+// Create the navigator
+const Stack = createNativeStackNavigator();
+
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+import { LogBox } from "react-native";
+LogBox.ignoreLogs(["AsyncStorage has been extracted from"]);
+
+import ShoppingLists from "./components/ShoppingLists";
+import Welcome from "./components/Welcome";
+
+const App = () => {
+  const firebaseConfig = {
+    apiKey: "AIzaSyCwX8peWg9oCr6bf5JPQmmDrfBHEiseIyU",
+    authDomain: "shopping-list-demo-fbfb0.firebaseapp.com",
+    projectId: "shopping-list-demo-fbfb0",
+    storageBucket: "shopping-list-demo-fbfb0.appspot.com",
+    messagingSenderId: "320982864209",
+    appId: "1:320982864209:web:78755689579042ed2c4370",
+    measurementId: "G-H0GQKSSV1S",
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  const db = getFirestore(app);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Welcome">
+        <Stack.Screen name="Welcome" component={Welcome} />
+        <Stack.Screen name="ShoppingLists">
+          {(props) => <ShoppingLists db={db} {...props} />}
+        </Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
